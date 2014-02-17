6b833d09d503f9dd86350dd37da1a15b53eb9a8b
Rodrigo Díaz <r2d2rigo@gmail.com>
2014-02-17 18:12:20 +0100

    Updated project and added to GitHub.

---
+++ MyFirstSharpDX/MyViewProvider.cs
@@ -0,0 +1,154 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2012 Rodrigo 'r2d2rigo' Díaz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Windows.ApplicationModel.Core;
+using Windows.System;
+using Windows.UI.Core;
+
+namespace MyFirstSharpDX
+{
+    /// <summary>
+    /// The view provider class that will handle all the view operations (update/draw).
+    /// </summary>
+    internal class MyViewProvider : IFrameworkView
+    {
+        private CoreWindow window;
+        private SharpDX.Direct3D11.Device1 device;
+        private SharpDX.Direct3D11.DeviceContext1 context;
+        private RenderTargetView backBuffer;
+        private SwapChain1 swapChain;
+
+        /// <summary>
+        /// This function is called before SetWindow, so we can't do much yet.
+        /// </summary>
+        /// <param name="applicationView"></param>
+        public void Initialize(CoreApplicationView applicationView)
+        {
+        }
+
+        /// <summary>
+        /// Now that we have a CoreWindow object, the DirectX device/context can be created.
+        /// </summary>
+        /// <param name="entryPoint"></param>
+        public void Load(string entryPoint)
+        {
+            // Get the default hardware device and enable debugging. Don't care about the available feature level.
+            SharpDX.Direct3D11.Device defaultDevice = new SharpDX.Direct3D11.Device(DriverType.Hardware, DeviceCreationFlags.Debug);
+
+            // Query the default device for the supported device and context interfaces.
+            device = defaultDevice.QueryInterface<SharpDX.Direct3D11.Device1>();
+            context = device.ImmediateContext.QueryInterface<DeviceContext1>();
+
+            // Query for the adapter and more advanced DXGI objects.
+            SharpDX.DXGI.Device2 dxgiDevice2 = device.QueryInterface<SharpDX.DXGI.Device2>();
+            SharpDX.DXGI.Adapter dxgiAdapter = dxgiDevice2.Adapter;
+            SharpDX.DXGI.Factory2 dxgiFactory2 = dxgiAdapter.GetParent<SharpDX.DXGI.Factory2>();
+
+            // Description for our swap chain settings.
+            SwapChainDescription1 description = new SwapChainDescription1()
+            {
+                // 0 means to use automatic buffer sizing.
+                Width = 0,
+                Height = 0,
+                // 32 bit RGBA color.
+                Format = Format.B8G8R8A8_UNorm,
+                // No stereo (3D) display.
+                Stereo = false,
+                // No multisampling.
+                SampleDescription = new SampleDescription(1, 0),
+                // Use the swap chain as a render target.
+                Usage = Usage.RenderTargetOutput,
+                // Enable double buffering to prevent flickering.
+                BufferCount = 2,
+                // No scaling.
+                Scaling = Scaling.None,
+                // Flip between both buffers.
+                SwapEffect = SwapEffect.FlipSequential,
+            };
+
+            // Generate a swap chain for our window based on the specified description.
+            swapChain = dxgiFactory2.CreateSwapChainForCoreWindow(device, new ComObject(window), ref description, null);
+
+            // Create the texture and render target that will hold our backbuffer.
+            Texture2D backBufferTexture = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            backBuffer = new RenderTargetView(device, backBufferTexture);
+
+            backBufferTexture.Dispose();
+        }
+
+        /// <summary>
+        /// Run our application until the user quits.
+        /// </summary>
+        public void Run()
+        {
+            // Make window active and hide mouse cursor.
+            window.PointerCursor = null;
+            window.Activate();
+
+            // Infinite loop to prevent the application from exiting.
+            while (true)
+            {
+                // Dispatch all pending events in the queue.
+                window.Dispatcher.ProcessEvents(CoreProcessEventsOption.ProcessAllIfPresent);
+
+                // Quit if the users presses Escape key.
+                if (window.GetAsyncKeyState(VirtualKey.Escape) == CoreVirtualKeyStates.Down)
+                {
+                    return;
+                }
+
+                // Set the backbuffer as the active rendertarget and clear it with the selected color.
+                context.OutputMerger.SetTargets(backBuffer);
+                context.ClearRenderTargetView(backBuffer, Color.Red);
+
+                // Present the current buffer to the screen.
+                swapChain.Present(1, PresentFlags.None);
+            }
+        }
+
+        /// <summary>
+        /// Sets the window where the app will be rendered.
+        /// </summary>
+        /// <param name="window">Our main window</param>
+        public void SetWindow(CoreWindow window)
+        {
+            this.window = window;
+        }
+
+        /// <summary>
+        /// Dispose all the created objects.
+        /// </summary>
+        public void Uninitialize()
+        {
+            swapChain.Dispose();
+            backBuffer.Dispose();
+            context.Dispose();
+            device.Dispose();
+        }
+    }
+}
